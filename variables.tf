99bff613459d365c0dca771b53c577e423aacd03
charanjit-dev <charanjit.singh@outlook.in>
2025-01-27 01:19:00 +0530

    code corrected

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "sg_base_name" {
 
 variable "sg_configs" {
   description = "Map of security group configurations."
-  type        = map(object({
+  type = map(object({
     name = string
     port = number
   }))
