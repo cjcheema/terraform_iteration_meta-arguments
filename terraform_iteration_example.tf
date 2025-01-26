b584506f4893e522a74c8d9c51dc7fa6c19343e0
charanjit-dev <charanjit.singh@outlook.in>
2025-01-27 00:45:01 +0530

    new files added

---
+++ terraform_iteration_example.tf
@@ -0,0 +1,12 @@
+# Invoke security group count module
+module "security_groups_count" {
+  source        = "./modules/sg_count"
+  number_of_sgs = var.number_of_sgs
+  sg_base_name  = var.sg_base_name
+}
+
+# Invoke security group for_each module
+module "security_groups_for_each" {
+  source     = "./modules/sg_for_each"
+  sg_configs = var.sg_configs
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "The AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
+}
