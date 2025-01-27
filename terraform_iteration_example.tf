06e1ebdd653677a35494d2a4f5ce12f1410ac71d
charanjit-dev <charanjit.singh@outlook.in>
2025-01-27 22:29:50 +0530

    Updated the code

--- terraform_iteration_example.tf
+++ terraform_iteration_example.tf
@@ -1,9 +1,9 @@
 # Invoke security group count module
-/*module "sg_count" {
+module "sg_count" {
   source        = "./modules/sg_count"
   number_of_sgs = var.number_of_sgs
   sg_base_name  = var.sg_base_name
-}*/
+}
 
 # Invoke security group for_each module
 module "security_groups_for_each" {
@@ -11,6 +11,7 @@ module "security_groups_for_each" {
   sg_configs = var.sg_configs
 }
 
+# Invoke security group count condition module
 module "sg_count_condition" {
   source    = "./modules/sg_count_condition"
   create_sg = var.create_sg
